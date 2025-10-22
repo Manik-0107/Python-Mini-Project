366b23686c3f5f54694d7790fb55363092911f8e
Manik-0107 <manikmondal0107@gmail.com>
2025-10-22 21:18:49 +0530

    Create 03 Filled Area Between Curves.py

---
+++ 03 Filled Area Between Curves.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 10, 500)
+y1 = np.sin(x)
+y2 = np.sin(x) + 0.5
+
+plt.fill_between(x, y1, y2, color='skyblue', alpha=0.4)
+plt.plot(x, y1, color='blue')
+plt.plot(x, y2, color='red')
+plt.title("Filled Area Between Curves")
+plt.show()
