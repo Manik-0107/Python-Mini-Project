7670fbe3e7b190496f1b262a67c652123452886b
Manik-0107 <manikmondal0107@gmail.com>
2025-11-26 20:26:14 +0530

    Create 04 Duplicate Folder Finder.py

---
+++ 04 Duplicate Folder Finder.py
@@ -0,0 +1,19 @@
+import os, hashlib
+
+path = input("Please Enter Folder Path: ")
+seen = {}
+
+for f in os.listdir(path):
+    data = open(os.path.join(path, f), "rb").read()
+    h = hashlib.md5(data).hexdigest()
+    if h in seen:
+        print("Duplicate File: ", f, "<->", seen[h])
+    else:
+        seen[h] = f
+
+
+
+"""
+Folder: E:\New folder
+Duplicate File:  New Text Document.txt <-> New Text Document (2).txt
+"""
